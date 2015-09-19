44d6be3cc6dbf611ea262859da84eda363e47c32
Adriaan Brands <adriaan_brands@live.nl>
2015-09-19 19:43:51 +0200

    Initial Commit

---
+++ IMDBInfo/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  IMDBInfo
+//
+//  Created by Adriaan Brands on 19-09-15.
+//  Copyright © 2015 Adriaan Brands. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
